e3c533d4057552e1283315419d828e503efb8d24
Thomas Brand <tom@trellis.ch>
2016-02-19 23:58:31 +0100

    initial add. install prerequisites and osrm-backend

---
+++ dist/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" -a -z "$SUDO" ]
+then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
+echo "install osrm-backend? (ctrl+c to abort)"
+read a
+
+apt-get -y install libboost-system1.54.0 libboost-filesystem1.54.0 libboost-program-options1.54.0 libboost-thread1.54.0 libexpat1 libluabind0.9.1 liblua5.2-0 libstxxl1 libtbb2
+dpkg -i osrm-backend_20160219-1_amd64.deb
+
+echo "done."
+dpkg -L osrm-backend | grep bin
+
