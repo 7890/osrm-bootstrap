68d886e39145786d22c229f401c8bf9e84270783
Thomas Brand <tom@trellis.ch>
2016-02-19 23:57:23 +0100

    remove sudo, add permissions check

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" -a -z "$SUDO" ]
+then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
 PREFIX="/usr/local"
 
 echo "PREFIX: $PREFIX"
 echo "uninstall osrm-backend? ctrl+c to abort"
 read a
 
-sudo rm -rf "$PREFIX"/include/osrm/
-sudo rm -f "$PREFIX"/bin/osrm-*
-sudo rm -f "$PREFIX"/lib/libosrm*
-sudo rm -f "$PREFIX"/lib/pkgconfig/libosrm.pc
+rm -rf "$PREFIX"/include/osrm/
+rm -f "$PREFIX"/bin/osrm-*
+rm -f "$PREFIX"/lib/libosrm*
+rm -f "$PREFIX"/lib/pkgconfig/libosrm.pc
